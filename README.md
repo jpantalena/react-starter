b5b06d173f53519ea9670500d335cbb70f93dade
Jack Pantalena <12242683+jpantalena@users.noreply.github.com>
2021-03-16 16:34:53 -0600

    update typo in setup.sh and update READM.md with initial setup instructions

--- README.md
+++ README.md
@@ -5,8 +5,13 @@ React starter project serving as a base for the best UI development possible!
 This project uses TypeScript and Webpack for an enhanced frontend development
 experience.
 
+## Start new project
+
+1. `git clone https://github.com/jpantalena/react-starter.git <project-name>`
+1. `./setup.sh`
+1. Follow prompts and enter `<project-name>` from first step.
+
 ## How to use
 
-1. (Optional - project re-naming from react-starter) `setup.sh`
 1. `npm install`
 1. `npm start`
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 read -p "Enter project name: " name
 
-read -p "Confirm? $name (y/n): " confirm && [[ $confirm == [yY] || exit 1
+read -p "Confirm? $name (y/n): " confirm && [[ $confirm == [yY] ]] || exit 1
 
 # Update package.json
 sed -i "" "s/react-starter/$name/g" package.json
@@ -11,7 +11,6 @@ sed -i "" "s/react-starter/$name/g" package.json
 sed -i "" "s/react-starter/$name/g" src/public/index.html
 
 # Update src/components/App/index.tsx
-
 sed -i "" "s/react-starter/$name/g" src/components/App/index.tsx
 
 echo "Finished setup."
