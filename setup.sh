1afb5e0a26dc56dab3a1eaa54f60878aa17416db
Jack Pantalena <12242683+jpantalena@users.noreply.github.com>
2021-03-16 16:46:01 -0600

    update setup.sh to match and replace new-project text

--- setup.sh
+++ setup.sh
@@ -5,12 +5,12 @@ read -p "Enter project name: " name
 read -p "Confirm? $name (y/n): " confirm && [[ $confirm == [yY] ]] || exit 1
 
 # Update package.json
-sed -i "" "s/react-starter/$name/g" package.json
+sed -i "" "s/new-project/$name/g" package.json
 
 # Update src/public/index.html
-sed -i "" "s/react-starter/$name/g" src/public/index.html
+sed -i "" "s/new-project/$name/g" src/public/index.html
 
 # Update src/components/App/index.tsx
-sed -i "" "s/react-starter/$name/g" src/components/App/index.tsx
+sed -i "" "s/new-project/$name/g" src/components/App/index.tsx
 
 echo "Finished setup."
