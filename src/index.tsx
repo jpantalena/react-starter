46075bd1b62d7865b951fface21736dd6051e8b1
Jack Pantalena <12242683+jpantalena@users.noreply.github.com>
2021-03-16 21:01:34 -0600

    use reset.css and update tsconfig

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
+import './stylesheets/reset'
 
 render(<App/>, document.getElementById("root"));
